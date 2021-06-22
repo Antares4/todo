1e577dc1565385ff65e0c01b27e9fb23c39e07cb
kurapikaaaa <petersheng2@outlook.com>
2021-06-22 13:43:02 +0800

    framework done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import Tasks from './components/tasks';
+import Header from './components/header';
+import Form from './components/form';
+import { useState } from 'react';
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id:1,
+        todo:"task1",
+        date:"2020-1-12",
+        urgent:false,
+    },
+    {
+        id:2,
+        todo:"task2",
+        date:"2020-1-13",
+        urgent:false,
+    }
+  ])  
+  const deleteTask = (id) =>{
+    setTasks(tasks.filter((task) => task.id!==id))
+  }
+  const createTask = (task) =>{
+    
+    const cid = Math.floor(Math.random()*20000) + 1 
+    var newTask={id:cid, ...task, urgent:false};
+    setTasks([...tasks, newTask]);
+    console.log(tasks)
+  }
+  const setUrgent = (id) =>{
+    console.log("urg", id)
+    setTasks(tasks.map((task) => task.id === id ? {...task, urgent:!task.urgent} : task))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      <Form createTask={createTask}/>
+    {tasks.length > 0 ? <Tasks tasks={tasks} deleteTask={deleteTask} setUrgent={setUrgent}/> : "nothing" }
     </div>
   );
 }
